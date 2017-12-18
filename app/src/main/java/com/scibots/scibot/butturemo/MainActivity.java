904257d610192a31c312c88735f87fea9d38f6fa
aniket965 <aniket965.as@gmail.com>
2017-12-18 22:08:12 +0530

    testing ir 🚀

--- app/src/main/java/com/scibots/scibot/butturemo/MainActivity.java
+++ app/src/main/java/com/scibots/scibot/butturemo/MainActivity.java
@@ -1,13 +1,47 @@
 package com.scibots.scibot.butturemo;
 
+import android.content.Context;
+import android.hardware.ConsumerIrManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    ConsumerIrManager mCIR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mCIR = (ConsumerIrManager) getSystemService(Context.CONSUMER_IR_SERVICE);
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+    public void IncVolume(View view) {
+        if (!mCIR.hasIrEmitter()) {
+            Log.e(TAG, "No IR Emitter found\n");
+            return;
+        }
+        ConsumerIrManager.CarrierFrequencyRange[] freqs = mCIR.getCarrierFrequencies();
+        for (ConsumerIrManager.CarrierFrequencyRange range : freqs) {
+           Log.d(TAG,String.format("    %d - %d\n", range.getMinFrequency(),
+                   range.getMaxFrequency()));
+        }
+    }
+    public void DecVolume(View view) {
+
     }
 }
